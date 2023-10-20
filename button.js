a9ef24177b6559ea80b1f6945e3d351f710fe413
JohnConstantinn <147560578+JohnConstantinn@users.noreply.github.com>
2023-10-20 12:14:34 -0400

    working button

---
+++ button.js
@@ -0,0 +1,8 @@
+let myVar = document.querySelector('.riley');
+let spiceButton = document.querySelector('.spiceButton');
+
+spiceButton.onclick = function(){
+    myVar.classList.toggle('riley');
+    myVar.classList.toggle('riley2');
+
+};
